b67d502a528553fbf7d9b7af1f7c08c8880198c2
HowardRoark_10 <aditya@iwizardsolutions.com>
2020-06-19 20:36:13 +0530

    introduction to python completed

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,27 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import sys
+
+def portfolio_cost(filename):
+    totalCost = 0.0
+    with open(filename, 'rt') as file:
+        headers = next(file)
+        try:
+            for line in file:
+                shares = int(line.split(',')[1])
+                price = float(line.split(',')[2])
+                totalCost += shares*price
+        except ValueError:
+            print('Unable to parse a line')
+    return totalCost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+
+print(f'Total cost to buy all shares is {cost:.2f}')
